5185dcb59ac0716f9ac4cc183c629763b1f9a66c
Miadog7Extra <petterkarstensen@gmail.com>
2024-10-10 14:41:06 +0200

    plan for buildings

---
+++ src/components/shade/buildings.tsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+
+//const Buildings: React.FC = () => {
+    // fetch lat long from resturant
+    // fetch azimuth, altitude from sunPosition
+
+    // random building at 10 meters in hight and 5 meters away
+    // this state should be occupied by the "worst" building for direct sun.
+    // must add trees, mountains, hills also.
+  //  const [building, setBuilding] = useState({height: 10, length: 5})
+
+//};
+
+
+//Starter med geopunkte.
+//finn ut hvordan solen kan blokkes med høyde og gradene.
+//start nærmest, fra punktek. Utvid gradevis i bredd og høyde i forhold til en funksjon som sikkert fungerer for sol og skygge.
--- src/components/shade/sunPosition.tsx
+++ src/components/shade/sunPosition.tsx
@@ -2,15 +2,15 @@ import React, { useState, useEffect } from 'react';
 import * as SunCalc from 'suncalc';
 
 const SunPosition: React.FC = () => {
+  // Should get lat, long from resturant.
   const [sunPosition, setSunPosition] = useState({ altitude: 0, azimuth: 0 });
   const [time, setTime] = useState(new Date());
-
 
   useEffect(() => {
     const currentTime = new Date();
     setTime(currentTime)
 
-    const position = SunCalc.getPosition(time, 59.911491, 10.757933);
+    const position = SunCalc.getPosition(time, 59.95610151740369, 10.725401777113094);
     
     const altitude = position.altitude * (180 / Math.PI);
     const azimuth = (position.azimuth * (180 / Math.PI) + 180) % 360;
