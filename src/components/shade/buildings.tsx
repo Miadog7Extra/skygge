4ac71d5fee472c1c89d773806b73555f7b9b904e
Miadog7Extra <petterkarstensen@gmail.com>
2024-10-10 15:01:32 +0200

    plan for buildings v2

--- src/components/shade/buildings.tsx
+++ src/components/shade/buildings.tsx
@@ -3,15 +3,25 @@ import React, { useState, useEffect } from 'react';
 //const Buildings: React.FC = () => {
     // fetch lat long from resturant
     // fetch azimuth, altitude from sunPosition
+    const long = 59.95610151740369 
+    const lat = 10.725401777113094
+    const azi = 199.22
+    const alti = 21.76
 
     // random building at 10 meters in hight and 5 meters away
     // this state should be occupied by the "worst" building for direct sun.
     // must add trees, mountains, hills also.
-  //  const [building, setBuilding] = useState({height: 10, length: 5})
+    //const [building, setBuilding] = useState({height: 10, length: 5})
+
+
 
 //};
 
 
-//Starter med geopunkte.
-//finn ut hvordan solen kan blokkes med høyde og gradene.
-//start nærmest, fra punktek. Utvid gradevis i bredd og høyde i forhold til en funksjon som sikkert fungerer for sol og skygge.
+// Starter med geopunkte og azimuth, altitude
+// etabler en linje fra geopunktet mot solen basert på azi og alti
+// start fra geopunktet og fortsett mot solen
+// hvis en byggning treffer linjen, sjekk om byggningens høyde er høyere enn solen på det punket (basert på mitt geopunkt)
+// bygning * tan(altitude)??
+// stopp når første hinder er funnet.
+// fortsett til ??, kommer kanskje an på sol altitude?
