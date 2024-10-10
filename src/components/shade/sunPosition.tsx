4498dfab9e9fc25848f5c9083d8dc3c7a5fe5947
Miadog7Extra <petterkarstensen@gmail.com>
2024-10-10 11:05:12 +0200

    correct azimuth degrees

--- src/components/shade/sunPosition.tsx
+++ src/components/shade/sunPosition.tsx
@@ -3,13 +3,17 @@ import * as SunCalc from 'suncalc';
 
 const SunPosition: React.FC = () => {
   const [sunPosition, setSunPosition] = useState({ altitude: 0, azimuth: 0 });
+  const [time, setTime] = useState(new Date());
+
 
   useEffect(() => {
-    const time = new Date("2024-10-07T15:00:00");
-    const position = SunCalc.getPosition(time, 40.7128, -74.0060);
+    const currentTime = new Date();
+    setTime(currentTime)
+
+    const position = SunCalc.getPosition(time, 59.911491, 10.757933);
     
     const altitude = position.altitude * (180 / Math.PI);
-    const azimuth = position.azimuth * (180 / Math.PI);
+    const azimuth = (position.azimuth * (180 / Math.PI) + 180) % 360;
     
     setSunPosition({ altitude, azimuth });
   }, []);
@@ -17,6 +21,7 @@ const SunPosition: React.FC = () => {
   return (
     <div>
       <h2>Sun Position</h2>
+      <p>Time: {time.toString()}</p>
       <p>Altitude: {sunPosition.altitude.toFixed(2)}°</p>
       <p>Azimuth: {sunPosition.azimuth.toFixed(2)}°</p>
     </div>
